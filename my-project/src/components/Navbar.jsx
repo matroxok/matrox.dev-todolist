d278c6f7eaa69964424d401461bf7816574c2a3a
Mateusz Kozera <mateusz.kozera@pwr.edu.pl>
2024-10-15 16:20:26 +0200

    created project structure

---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<>
+			<div className='w-screen h-16 bg-navbar grid grid-cols-3 items-center justify-items-center'>
+				<h1 className=''>test</h1>
+				<h1 className=''>test</h1>
+				<h1 className=''>test</h1>
+			</div>
+		</>
+	)
+}
+
+export default Navbar
